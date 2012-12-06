f574b2e8a45d62a69af3c9923a25e5bce2b7f81d
David Strack <davidstrack@gmail.com>
2012-12-06 13:37:45 -0600

    don't rsync on the entire project

--- libraries/cli/cmd/sync.php
+++ libraries/cli/cmd/sync.php
@@ -191,14 +191,12 @@ function _rsync($options) {
 	ob_start();
 	
 	$ssh = BurritoConfig::env('sync', 'ssh');
+	$port = ($ssh['port']) ? $ssh['port'] : 22;
 	
 	echo sprintf(
 		'-rvtzp --exclude="backups/*" --exclude="cache/*" --exclude="tmp/*" --exclude="trash/*" --progress --rsh="ssh %s" %s',
-		_ssh(array_merge(
-			BurritoConfig::env('sync', 'ssh'),	  	
-			array('path'=>$options['remote_dir'])
-		)),
-		dirname(__C5__.$options['local_dir'])
+		'-p'.$port,
+		$ssh['user'].'@'.$ssh['host'].':'.$options['remote_dir'].' '.__C5__.$options['local_dir']
 	);
 	
 	return ob_get_clean();
